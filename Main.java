01deee1d1ede70a09934387357c1c2bb099e3145
Zx <148573313+zx-0b111@users.noreply.github.com>
2025-05-02 18:54:08 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
-
     public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         SHA256.Algoritmo();
     }
